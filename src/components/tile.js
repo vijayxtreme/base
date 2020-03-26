38a07ff1a826ec94867748402cede503fbaba8e4
vijayxtreme <vijaymenonx@gmail.com>
2020-03-25 21:22:19 -0700

    More padding on left right, cleaner midsection

--- src/components/tile.js
+++ src/components/tile.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Tile(props){
     const {content} = props
     return (
-        <div className="tile col-sm">
+        <div className="tile col-sm col-xs">
             {content}
         </div>
     )
